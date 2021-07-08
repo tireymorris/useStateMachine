627e9a5da941b857fd4b2521d1d58f6a88862f98
Ty Morris <tireymorris@gmail.com>
2021-07-08 16:56:24 -0500

    Add builder pattern

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,70 @@
 import React, { useReducer } from 'react';
 
-export type StateMachineSpec<State extends string, Event extends string> = Record<State, Partial<Record<Event, State>>>;
+export type StateMachineSpec<
+    State extends string,
+    Event extends string
+> = Record<State, Partial<Record<Event, State>>>;
 
 export interface StateMachine<State extends string, Event extends string> {
-	initialState: State;
-	machineSpec: StateMachineSpec<State, Event>;
+    initialState: State;
+    machineSpec: StateMachineSpec<State, Event>;
 }
 
-export type StateMachineReducer<State extends string, Event extends string> = (state: State, event: Event) => State | undefined;
+export type StateMachineReducer<State extends string, Event extends string> = (
+    state: State,
+    event: Event
+) => State | undefined;
 
-export const createStateMachine = <State extends string, Event extends string>(spec: StateMachineSpec<State, Event>) => {
-	return function reducer(state: State, event: Event) {
-		if (spec[state] && spec[state][event]) {
-			const newState = spec[state][event] as State;
+export const createStateMachine = <State extends string, Event extends string>(
+    spec: StateMachineSpec<State, Event>
+) => {
+    return function reducer(state: State, event: Event) {
+        if (spec[state] && spec[state][event]) {
+            const newState = spec[state][event] as State;
 
-			return newState;
-		}
+            return newState;
+        }
 
-		throw new Error(`Could not transition states from ${state} with event ${event}`);
-	  }
-}
+        throw new Error(
+            `Could not transition states from ${state} with event ${event}`
+        );
+    };
+};
 
+export const useStateMachine = <State extends string, Event extends string>(
+    spec: StateMachineSpec<State, Event>,
+    initialState: State
+): [State, React.Dispatch<Event>] => {
+    const reducer = createStateMachine<State, Event>(spec);
 
-export const useStateMachine = <State extends string, Event extends string>(spec: StateMachineSpec<State, Event>, initialState: State): [State, React.Dispatch<Event>] => {
-	const reducer = createStateMachine<State, Event>(spec);
-
-	return useReducer(reducer, initialState);
-}
+    return useReducer(reducer, initialState);
+};
 
 export default useStateMachine;
+
+export const createStateMachineBuilder = <
+    State extends string,
+    Event extends string
+>() => {
+    const spec: StateMachineSpec<State, Event> = {} as StateMachineSpec<
+        State,
+        Event
+    >;
+
+    return {
+        withTransition: function (
+            fromState: State,
+            byEvent: Event,
+            toState: State
+        ) {
+            if (!spec[fromState]) {
+                spec[fromState] = {};
+            }
+            spec[fromState][byEvent] = toState;
+
+            return this;
+        },
+        buildWithInitialState: (initialState: State) =>
+            useStateMachine(spec, initialState),
+    };
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,18 +1,22 @@
-import { createStateMachine, StateMachineSpec, useStateMachine } from "../src";
-import { renderHook, act } from '@testing-library/react-hooks'
+import {
+    createStateMachine,
+    createStateMachineBuilder,
+    StateMachineSpec,
+    useStateMachine,
+} from '../src';
+import { renderHook, act } from '@testing-library/react-hooks';
 
 type State = 'booting' | 'started' | 'ended';
 type Event = 'booted' | 'failed';
 
-const spec: StateMachineSpec<State, Event> = {
-	'booting': {
-		'booted': 'started'
-	},
-	'started': {
-		'failed': 'ended'
-	},
-	'ended': {
-	}
+const spec = {
+    booting: {
+        booted: 'started',
+    },
+    started: {
+        failed: 'ended',
+    },
+    ended: {},
 };
 
 test('reducer functions as expected', () => {
@@ -20,20 +24,40 @@ test('reducer functions as expected', () => {
 
     expect(dispatch('booting', 'booted')).toEqual('started');
     expect(dispatch('started', 'failed')).toEqual('ended');
-	expect(() => dispatch('ended', 'booted')).toThrow();
-})
+    expect(() => dispatch('ended', 'booted')).toThrow();
+});
 
 test('hook functions correctly', () => {
-	const { result } = renderHook(() => useStateMachine(spec, 'booting'))
-	const [_, dispatch] = result.current;
+    const { result } = renderHook(() => useStateMachine(spec, 'booting'));
+    const [_, dispatch] = result.current;
 
-	act(() => {
-		dispatch('booted')
-	})
-	expect(result.current[0]).toBe('started')
+    act(() => {
+        dispatch('booted');
+    });
+    expect(result.current[0]).toBe('started');
 
-	act(() => {
-		dispatch('failed')
-	})
-	expect(result.current[0]).toBe('ended')
-})
+    act(() => {
+        dispatch('failed');
+    });
+    expect(result.current[0]).toBe('ended');
+});
+
+test('can use builder pattern', () => {
+    const { result } = renderHook(() =>
+        createStateMachineBuilder()
+            .withTransition('booting', 'booted', 'started')
+            .withTransition('started', 'failed', 'ended')
+            .buildWithInitialState('booting')
+    );
+    const [_, dispatch] = result.current;
+
+    act(() => {
+        dispatch('booted');
+    });
+    expect(result.current[0]).toBe('started');
+
+    act(() => {
+        dispatch('failed');
+    });
+    expect(result.current[0]).toBe('ended');
+});
